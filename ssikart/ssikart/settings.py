6692a4ef565ac4234932d8f6dfe3a6b3830aa2c3
Sumit S Chawla <sam.chawla26@gmail.com>
2022-05-05 09:28:50 +0530

    Apps created and static files updated
    - Static files updated
    - Category App Created
    - Accounts App Created
    - Using Custom User Model
    - Added AccountUser in admin

--- ssikart/ssikart/settings.py
+++ ssikart/ssikart/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Apps
+    'accounts',
+    'category',
     'home',
 ]
 
@@ -115,14 +117,18 @@ USE_I18N = True
 USE_TZ = True
 
 
+# AUTH USER MODEL
+
+AUTH_USER_MODEL = 'accounts.Account'
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = []
-print(STATIC_ROOT)
-print("BASE:", BASE_DIR)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
