5e06c8517b44b00f762edff6134623072403b708
Sumit S Chawla <sam.chawla26@gmail.com>
2022-06-20 09:18:38 +0530

    WIP:Registration

---
+++ ssikart/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from accounts.views import register, login, logout
+
+
+urlpatterns = [
+    path('register/', register, name='registration'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+]
+
