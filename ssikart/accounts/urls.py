26aa409e33c8d6f625c37cb8e9b06293a16af3de
Sumit S Chawla <sam.chawla26@gmail.com>
2022-06-28 09:14:27 +0530

    Forget Password functionality added

--- ssikart/accounts/urls.py
+++ ssikart/accounts/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 
-from accounts.views import activate, login, logout, register
+from accounts.views import (activate, dashboard, forgot_password, login,
+                            logout, register, reset_password,
+                            resetpassword_validate)
 
 urlpatterns = [
     path("register/", register, name="registration"),
     path("login/", login, name="login"),
     path("logout/", logout, name="logout"),
     path("activate/<uidb64>/<token>", activate, name="activate"),
+    path("dashboard/", dashboard, name="dashboard"),
+    # Forget Password Urls
+    path("forgot-password/", forgot_password, name="forgot_password"),
+    path("reset-password/", reset_password, name="reset_password"),
+    path(
+        "resetpassword_validate/<uidb64>/<token>/",
+        resetpassword_validate,
+        name="resetpassword_validate",
+    ),
 ]
--- ssikart/accounts/views.py
+++ ssikart/accounts/views.py
@@ -85,7 +85,8 @@ def login(request):
 
         if user:
             auth.login(request, user)
-            return redirect("ssi-home")
+            messages.success(request, "You are now logged in!")
+            return redirect("dashboard")
         else:
             messages.error(request, "Invalid Credentials!")
             return redirect("login")
@@ -98,3 +99,72 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "You are logged out successfully!")
     return redirect("login")
+
+
+@login_required(login_url="login")
+def dashboard(request):
+    return render(request, "accounts/dashboard.html")
+
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+
+            # RESET PASSWORD SEND
+            current_site = get_current_site(request)
+            mail_subject = 'SSI Shop | Reset your password'
+            message = render_to_string('accounts/reset_password_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email= EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'Password reset email is sent to you email address.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account does not exist')
+            return redirect('forgot_password')
+
+    return render(request, 'accounts/forgot_password.html')
+
+
+def reset_password(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password reset successful')
+            return redirect('login')
+        else:
+            messages.error(request, 'Password do not match!')
+            return redirect('reset_password')
+    else:
+        return render(request, 'accounts/reset_password.html')
+
+
+def resetpassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request, 'Please reset your password')
+        return redirect('reset_password')
+    else:
+        messages.success(request, 'This link is expired!!!!')
+        return redirect('login')
